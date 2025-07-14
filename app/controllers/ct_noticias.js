80e717ddf0eda08778592aecf9f8829fbca68175
Luiz Gustavo Dominisini <dominizi.luiz@gmail.com>
2025-07-14 17:56:21 -0300

    feat: adicionando Joi para validação

--- app/controllers/ct_noticias.js
+++ app/controllers/ct_noticias.js
@@ -1,3 +1,14 @@
+const Joi = require("joi");
+const joi = require("joi");
+
+const noticiaSchema = Joi.object({
+    titulo: Joi.string().min(3).max(100).required(),
+    noticia: Joi.string().required(),
+    resumo: Joi.string().required(),
+    autor: Joi.string().min(3).max(50).required(),
+    data_noticia: Joi.date().required(),
+});
+
 module.exports.noticias = function (app, req, res) {
     const connection = app.config.db_connection();
     const noticiasDAO = new app.app.models.noticias_dao(connection);
@@ -25,10 +36,16 @@ module.exports.noticia = function (app, req, res) {
 };
 
 module.exports.noticiaSalvar = function (app, req, res) {
+    const { error, value } = noticiaSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
     const connection = app.config.db_connection();
     const noticiasDAO = new app.app.models.noticias_dao(connection);
 
-    noticiasDAO.setNoticiaSalvar(req.body, function (error, result) {
+    noticiasDAO.setNoticiaSalvar(value, function (error, result) {
         if (error) {
             return res.status(500).json({ error: "Failed to save news" });
         }
